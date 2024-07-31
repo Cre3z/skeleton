2f254038b0199ee31ee1aca71645bc88265f4aec
crezell <cnortje@hotmail.com>
2024-07-31 21:49:35 +0200

    recipe intro added

--- locales/allLocales.js
+++ locales/allLocales.js
@@ -1,6 +1,13 @@
 export default {
     "en" : {
-        "title" : "Sichuan Boiled Beef (水煮牛肉 – Shuizhu Niurou)",
-        "intro": "Some intro",
+        "title" : "Sichuan Boiled Beef",
+        "title-zh": "(水煮牛肉 – Shuizhu Niurou)",
+        "original-recipe-link": "The original post for this recipe can be found here.",
+        "intro-part-one": "Welcome to my recipe page. My name is Crezell, and I'm excited to share with you one of my all-time favorite dishes from China: Sichuan Beef Hot Pot. This dish is a vibrant and flavorful representation of Sichuan cuisine, known for its bold and spicy flavors.",
+        "intro-part-two": "In my free time, I study the Chinese language, and through this journey, I've developed a deep appreciation for Chinese culture and cuisine. Beef hot pot stands out as my favorite dish, and I'm thrilled to bring a taste of China to you.",
+        "intro-part-three": "This webpage is part of my project list for the Odin Project, where I'm honing my web development skills. I'm using the HTML boilerplate framework called Skeleton and focusing on creating clean, semantic HTML to build this site. I hope you enjoy exploring this recipe as much as I enjoy making and eating it!",
+        "recipe-intro-one": "Learn how to make hot and numbing Sichuan style water boiled beef (Shui Zhu Niu Rou) at home. Sichuan style water boiled beef is one of my favorite dishes in restaurants. If you ever tasted it in China, you will crazily love the tenderness of the beef slices and shocked by how well they are flavored in the hot and red broth.",
+        "recipe-intro-two": "“Water boiling” (水煮) is a unique cooking method originated in Sichuan cuisine. Of course, delicious beef slices cannot be achieved via “cooking in water”. The meat (fish, pork and beef) is well coated with a “protection shell” made from water and starch. This special layer of shell, on one hand, prevents the beef from being spoiled by the hot broth and on the other hand helps to keep the juice inside.",
+        "recipe-intro-three": "Another surprise of the dish, in addition to the beef slice, is the vegetables at the bottom. They can absorb enough flavor form the broth, even though they are not boiled in the broth directly. Popular options about bottom vegetables are cabbage, bean sprouts, celery, mushrooms and lettuce head."
     }
 }
