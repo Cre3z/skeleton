5d717ac492e507dae34b7adad253739bc0e95313
crezell <cnortje@hotmail.com>
2024-08-01 12:22:41 +0200

    removed loader

--- js/localization.js
+++ js/localization.js
@@ -5,17 +5,15 @@ const initPageLocale = () => {
     main.style.display = 'none'
 
     let language = document.documentElement.lang,
-    nodes = document.querySelectorAll('[data-l10n]'),
-    i = nodes.length
+        nodes = document.querySelectorAll('[data-l10n]'),
+        i = nodes.length
 
     while (i--) {
         let key = nodes[i].getAttribute('data-l10n');
         nodes[i].innerHTML = l10n[language][key];
     }
 
-    let loader = document.getElementById('loader')
     main.style.display = 'block'
-    loader.style.display = 'none'
     document.title = l10n[language]['title']
 }
 
